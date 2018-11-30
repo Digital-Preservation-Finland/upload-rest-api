c8a36d16d8669226d7cb20294e9f3f22b53fd627
Juho Kuisma <juho.kuisma@csc.fi>
2018-11-30 10:41:26 +0200

    TPASPKT-180 Database tests

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,12 +1,78 @@
 """Unit tests for database module"""
+import re
+import binascii
+
 import upload_rest_api.database as db
 
 
 def test_create_user(user):
-    """Test creation of of new user
+    """Test creation of new user
     """
     db = user.users
     user.username = "test"
     user.create()
 
-    assert db.find_one({"_id": "test"}) is not None
+    user_dict = db.find_one({"_id": "test"})
+
+    assert user_dict is not None
+    assert user.exists()
+    assert user_dict == user.get()
+
+    salt = user_dict["salt"]
+    assert len(salt) == 20
+
+    quota = user_dict["quota"]
+    assert quota == 5 * 1024**3
+
+    digest = user_dict["digest"]
+    assert len(digest) == 64
+
+
+def test_delete_user(user):
+    """Test deletion of user
+    """
+    db = user.users
+
+    db.insert_one({"_id": "test_user"})
+    db.insert_one({"_id": "test_user2"})
+
+    user.delete()
+
+    assert db.find_one({"_id": "test_user"}) is None
+    assert db.find_one({"_id": "test_user2"}) is not None
+
+
+def test_quota(user):
+    """Test get_quota() and set_quota() functions
+    """
+    db = user.users
+    db.insert_one({"_id": "test_user", "quota": 5 * 1024**3})
+
+    # Get
+    assert user.get_quota() == 5 * 1024**3
+    
+    # Set
+    user.set_quota(0)
+    assert db.find_one({"_id": "test_user"})["quota"] == 0
+
+
+def test_get_random_string():
+    """Test that _get_random_string() returns random strings
+    of given lenght with only ascii letters and digits
+    """
+    strings = set()
+
+    for _ in range(1000):
+        string = db._get_random_string(20)
+
+        assert len(string) == 20
+        assert re.match("^[A-Za-z0-9_-]*$", string)
+        assert string not in strings
+        strings.add(string)
+
+
+def test_hash_passwd():
+    """Test that salting and hashing returns the correct digest
+    """
+    digest = binascii.hexlify(db.hash_passwd("test", "test")[:16]) 
+    assert digest == "8809fd1f1e620cf1156353571199e227"
