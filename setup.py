b829fa94cc0122f206ab3f47eeace19bba8072ba
Juho Kuisma <juho.kuisma@csc.fi>
2019-01-23 16:52:56 +0200

    TPASPKT-159 adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Install upload-rest-api package"""
+from setuptools import setup, find_packages
+
+from version import get_version
+
+
+def main():
+    """Install upload-rest-api"""
+    setup(
+        name='upload-rest-api',
+        packages=find_packages(exclude=['tests', 'tests.*']),
+        version=get_version(),
+        install_requires=[
+            "flask",
+            "pymongo",
+            "file-magic",
+            "metax_access",
+        ],
+        dependency_links=[
+            'git+https://gitlab.csc.fi/dpres/metax-access.git'
+            '@develop#egg=metax-access-0.0'
+        ]
+    )
+
+if __name__ == '__main__':
+    main()
