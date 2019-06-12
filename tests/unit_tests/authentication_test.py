4d78f0cdb124b1bd4313c5b440c31638c6a8ee2f
Juho Kuisma <juho.kuisma@csc.fi>
2019-06-12 09:58:52 +0300

    TPASPKT-117 replacing _slow_equals with hmac.compare_digest

--- tests/unit_tests/authentication_test.py
+++ tests/unit_tests/authentication_test.py
@@ -2,14 +2,6 @@
 import upload_rest_api.authentication as auth
 
 
-def test_slow_equals():
-    """Test _slow equals() function"""
-    assert auth._slow_equals("test", "test")
-    assert not auth._slow_equals("test1", "test")
-    assert auth._slow_equals("", "")
-    assert not auth._slow_equals("", "test")
-
-
 def test_auth_user(user):
     """Test _auth_user() function
     """
--- upload_rest_api/authentication.py
+++ upload_rest_api/authentication.py
@@ -1,32 +1,10 @@
 """Module for authenticating users
 """
+from hmac import compare_digest
+
 from flask import request, abort
 
 from upload_rest_api import database as db
-
-
-def _slow_equals(hash1, hash2):
-    """Function to compare hashes in O(n) time no matter
-    how many common bytes they have. Bitwise XOR (^)
-    is used for comparison to avoid branching and terminating
-    immediately when difference is spotted. This function is used
-    to negate timing attacks:
-
-    https://crypto.stanford.edu/~dabo/papers/ssl-timing.pdf
-
-    :param hash1: First hash to compare
-    :param hash2: Second hash to compare
-    :returns: True if identical else False
-    """
-
-    # Iterate until the end of the shorter hash
-    hash_len = min(len(hash1), len(hash2))
-
-    diff = len(hash1) ^ len(hash2)
-    for i in range(hash_len):
-        diff |= ord(hash1[i]) ^ ord(hash2[i])
-
-    return diff == 0
 
 
 def _auth_user(username, password, user=None):
@@ -39,12 +17,12 @@ def _auth_user(username, password, user=None):
     else:
         # Calculate digest even if user does not exist to avoid
         # leaking information about which users exist
-        return _slow_equals("hash"*16, db.hash_passwd("passwd", "salt"))
+        return compare_digest("hash"*16, db.hash_passwd("passwd", "salt"))
 
     salt = user["salt"]
     digest = user["digest"]
 
-    return _slow_equals(digest, db.hash_passwd(password, salt))
+    return compare_digest(digest, db.hash_passwd(password, salt))
 
 
 def authenticate():
