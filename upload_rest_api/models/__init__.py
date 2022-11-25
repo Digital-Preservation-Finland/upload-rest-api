916e08758619b1fa909a9f8b00fa437a62fa6e58
Janne Pulkkinen <janne.pulkkinen@csc.fi>
2022-11-25 11:42:22 +0000

    TPASPKT-914 Defer MongoDB connection
    Connect to MongoDB during the first operation. This ensures that
    MongoClient isn't created before a fork, which can lead to unexpected
    behavior.

--- upload_rest_api/models/__init__.py
+++ upload_rest_api/models/__init__.py
@@ -29,7 +29,11 @@ __all__ = (
 try:
     connect(
         host=f"mongodb://{CONFIG['MONGO_HOST']}:{CONFIG['MONGO_PORT']}/upload",
-        tz_aware=True
+        tz_aware=True,
+        # Connect on first operation instead of instantly.
+        # This is to prevent MongoClient from being created before a fork,
+        # which leads to unexpected behavior.
+        connect=False
     )
 except KeyError:
     logging.error(
