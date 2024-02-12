23fea9d10f6a1c0910327d043f8aa6c3835d713e
Janne Pulkkinen <janne.pulkkinen@csc.fi>
2024-02-12 14:21:58 +0000

    TPASPKT-1265 Implement `get_file_checksums`
    Create `get_file_checksums` function that allows calculating multiple
    checksums at the same time while only reading the file once. This will
    be used in the background checksum verification task to prevent
    redundant work.

--- upload_rest_api/checksum.py
+++ upload_rest_api/checksum.py
@@ -3,8 +3,9 @@ import base64
 import hashlib
 import json
 import pickle
-
 import ssl
+from typing import Iterable
+
 import rehash
 
 from upload_rest_api.redis import get_redis_connection
@@ -98,6 +99,45 @@ def calculate_incr_checksum(algorithm, path, finalize=False):
     return hash_obj.hexdigest()
 
 
+def get_file_checksums(algorithms: Iterable[str], path: str) -> dict:
+    """
+    Calculate the file checksum using given algorithms for a file
+
+    :param algorithms: Cryptographic hash algorithms used to calculate
+                       the checksums
+    :param path: Path to the file
+
+    :returns: Checksums as a {algorithm: checksum} dict
+    """
+    algorithms = list(algorithms)
+
+    hash_objs = []
+
+    for algorithm in algorithms:
+        try:
+            hash_func = getattr(
+                hashlib,
+                HASH_FUNCTION_ALIASES[algorithm.lower()]
+            )
+            hash_obj = hash_func()
+            hash_objs.append(hash_obj)
+        except KeyError as exc:
+            raise ValueError(
+                f"Hash function '{algorithm}' not recognized"
+            ) from exc
+
+    with open(path, "rb") as file_:
+        # Read the file in 1 MB chunks
+        for chunk in iter(lambda: file_.read(1024 * 1024), b""):
+            for hash_obj in hash_objs:
+                hash_obj.update(chunk)
+
+    return {
+        algorithm: hash_obj.hexdigest()
+        for algorithm, hash_obj in zip(algorithms, hash_objs)
+    }
+
+
 def get_file_checksum(algorithm, path):
     """
     Calculate the file checksum using a given algorithm for a file.
@@ -110,17 +150,4 @@ def get_file_checksum(algorithm, path):
 
     :returns: Checksum as a hex string
     """
-    try:
-        hash_func = getattr(hashlib, HASH_FUNCTION_ALIASES[algorithm.lower()])
-        hash_obj = hash_func()
-    except KeyError as exc:
-        raise ValueError(
-            f"Hash function '{algorithm}' not recognized"
-        ) from exc
-
-    with open(path, "rb") as file_:
-        # Read the file in 1 MB chunks
-        for chunk in iter(lambda: file_.read(1024 * 1024), b""):
-            hash_obj.update(chunk)
-
-    return hash_obj.hexdigest()
+    return get_file_checksums([algorithm], path)[algorithm]
